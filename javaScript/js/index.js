d25b80596cc337fe804410b72f97a2eeb22f2191
carlitoscix <amazoncix@gmail.com>
2024-05-15 08:41:12 -0500

    Resplado19

--- javaScript/js/index.js
+++ javaScript/js/index.js
@@ -1,11 +1,12 @@
-funtion compara() {
-    var a = 2;
-    var b = 5;
-    if (a==b) {
+function compara() {
+    var a=2;
+    var b=2;
+    if (a==b){
         alert("a y b son iguales");
     }else {
             alert("a y b son diferentes");
         }
+    
     
 }
 
